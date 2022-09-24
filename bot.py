903f77ba980048389a79d18b0d0c882dd0ccf34a
lecovi <lecovi@ac.python.org.ar>
2022-09-24 15:14:51 -0300

    chore: TelegramBot uses Class based helper

--- bot.py
+++ bot.py
@@ -1,10 +1,12 @@
-from multiprocessing import allow_connection_pickling
-from multiprocessing.connection import answer_challenge
+import html
 import re
 import logging
+import traceback
+import json
 
 from emoji import emojize
 from telegram import Update
+from telegram.constants import ParseMode
 from telegram.ext import (
     ApplicationBuilder,
     CommandHandler,
@@ -13,17 +15,16 @@ from telegram.ext import (
     filters,
 )
 
-from bloodpressure_monitor_bot.gapi.helpers import (
-    sheet, SPREADSHEET_ID, RANGE, SPREADSHEET_URL, add_record
-    )
+from bloodpressure_monitor_bot.gapi.helpers import BloodPressureGoogleSheet
 
 
 BLOODPRESSURE_REGEX_PATTERN = "(\d{2,3})\/(\d{2,3})( (\d{2,3}))?"
-
+DEVELOPER_CHAT_ID = 9691064
+SERVICE_ACCOUNT_FILE = 'credentials/primera.json'
 
 # Enable logging
 logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.DEBUG
 )
 
 logger = logging.getLogger(__name__)
@@ -37,15 +38,13 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Send a message when the command /start is issued."""
     user = update.effective_user
 
-    result = sheet.values().get(
-        spreadsheetId=SPREADSHEET_ID, 
-        range=RANGE
-    ).execute()
+    sheet = BloodPressureGoogleSheet(service_account_file=SERVICE_ACCOUNT_FILE)
+    context.bot_data["sheet"] = sheet
 
-    values = result.get('values', [])
+    values = sheet.get_records()
 
     welcome_message = emojize(f"""Hi {user.mention_html()}!
-Connected with GOOGLE :link: <a href="{SPREADSHEET_URL}">Spreadsheet</a>. 
+Connected with GOOGLE :link: <a href="{sheet.url}">Spreadsheet</a>. 
 Spreasheet has <b>{len(values)-1} records</b> :chart_increasing:.
 """, language='alias')
 
@@ -69,8 +68,8 @@ async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         if heart_beat:
             heart_beat = match.group(4)
 
-        result = add_record(
-            sheet=sheet,
+        sheet = context.bot_data["sheet"]
+        result = sheet.add_record(
             systolic=systolic,
             diastolic=diastolic,
             heart_beat=heart_beat
@@ -85,10 +84,39 @@ async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(answer)
 
 
+async def error_handler(update: object, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Log the error and send a telegram message to notify the developer."""
+    # Log the error before we do anything else, so we can see it even if something breaks.
+    logger.error(msg="Exception while handling an update:", exc_info=context.error)
+
+    # traceback.format_exception returns the usual python message about an exception, but as a
+    # list of strings rather than a single string, so we have to join them together.
+    tb_list = traceback.format_exception(None, context.error, context.error.__traceback__)
+    tb_string = "".join(tb_list)
+
+    # Build the message with some markup and additional information about what happened.
+    # You might need to add some logic to deal with messages longer than the 4096 character limit.
+    update_str = update.to_dict() if isinstance(update, Update) else str(update)
+    message = (
+        f"An exception was raised while handling an update\n"
+        f"<pre>update = {html.escape(json.dumps(update_str, indent=2, ensure_ascii=False))}"
+        "</pre>\n\n"
+        f"<pre>context.chat_data = {html.escape(str(context.chat_data))}</pre>\n\n"
+        f"<pre>context.user_data = {html.escape(str(context.user_data))}</pre>\n\n"
+        f"<pre>{html.escape(tb_string)}</pre>"
+    )
+
+    # Finally, send the message
+    await context.bot.send_message(
+        chat_id=DEVELOPER_CHAT_ID, text=message, parse_mode=ParseMode.HTML
+    )
+
+
 app = ApplicationBuilder().token(TOKEN).build()
 
 app.add_handler(CommandHandler("hello", hello))
 app.add_handler(CommandHandler("start", start))
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+app.add_error_handler(error_handler)
 
 app.run_polling()
