0fef8b654a6b24659b4f0384fe4c768ca6a1cb8b
lecovi <lecovi@ac.python.org.ar>
2022-10-08 12:39:37 -0300

    feat: add CRUD CLI for Users

--- bloodpressure_monitor_bot/db/cli.py
+++ bloodpressure_monitor_bot/db/cli.py
@@ -1,11 +1,13 @@
 import typer
 from rich import print
-from sqlmodel import SQLModel
+from sqlmodel import SQLModel, Session, select
 
-from .core import engine, DB_URI
+from .core import engine, DB_URI, User
 
 
 app = typer.Typer()
+users = typer.Typer()
+app.add_typer(users, name="user")
 
 
 @app.command()
@@ -18,3 +20,72 @@ def create():
 def delete():
     SQLModel.metadata.drop_all(engine)
     print(f"Deleting database :right_arrow: {DB_URI}")
+
+
+@users.command()
+def add(name=None, telegram_handler=None, email=None, sheet_id=None):
+    user = User(name=name, telegram_handler=telegram_handler, email=email, sheet_id=sheet_id)
+    with Session(engine) as session:
+        session.add(user)
+        session.commit()
+    print(user)
+
+
+@users.command()
+def get(id):
+    with Session(engine) as session:
+        statement = select(User).where(User.id == id)
+        user = session.exec(statement).first()
+    print(user)
+
+
+@users.command()
+def get_all():
+    with Session(engine) as session:
+        statement = select(User)
+        users = session.exec(statement)
+        for user in users:
+            print(user)
+
+
+@users.command()
+def update(id, name=None, telegram_handler=None, email=None, sheet_id=None):
+    with Session(engine) as session:
+        # Get the user
+        statement = select(User).where(User.id == id)
+        user = session.exec(statement).first()
+        if user is None:
+            print(":confused_face: User does not exists!")
+            return 
+
+        # Update values
+        if name is not None:
+            user.name = name
+        if telegram_handler is not None:
+            user.telegram_handler = telegram_handler
+        if email is not None:
+            user.email = email
+        if sheet_id is not None:
+            user.sheet_id = sheet_id
+
+        # Save to DB
+        session.add(user)
+        session.commit()
+
+        print(user)
+
+
+@users.command()
+def delete(id):
+    with Session(engine) as session:
+        # Get the user
+        statement = select(User).where(User.id == id)
+        user = session.exec(statement).first()
+        if user is None:
+            print(":confused_face: User does not exists!")
+            return 
+
+        session.delete(user)
+        session.commit()
+
+    print(f":wastebasket: Deleted user", user)
--- bloodpressure_monitor_bot/db/core.py
+++ bloodpressure_monitor_bot/db/core.py
@@ -16,6 +16,12 @@ class User(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     name: str
     telegram_handler: str
-    email: str = Field(nullable=False, index=True, unique=True)
+    email: str = Field(index=True, unique=True)
     sheet_id: str
     last_conection: datetime = Field(default_factory=datetime.utcnow, nullable=False)
+
+    # def __str__(self):
+    #     return f"({self.__class__.__name__} {self.email=} {self.telegram_handler=} {self.sheet_id=})"
+
+    # def __repr__(self):
+    #     return f"<{self.__class__.__name__} {self.email=}>"
